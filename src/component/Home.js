5aca1c22dd66212f3f7d788956979e59aba43226
ZacYou <zyou@calstatela.edu>
2020-04-27 18:09:52 -0700

    last fix

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Toolbar from './Toolbar';
 import './css_doc/Home.css';
+import './css_doc/Toolbar.css';
 import Content from './Content'
 import Project from './Project';
 import About from './About';
@@ -13,9 +14,14 @@ export default class Home extends Component {
   constructor(){
     super();
     this.windowScroll = this.windowScroll.bind(this);
+    this.navListener = this.navListener.bind(this);
   }
   componentDidMount(){
+    this.navListener();
     this.windowScroll();
+  }
+  componentWillUnmount(){
+    this.navListener();
   }
   windowScroll(){
     let main = document.getElementById('main');
@@ -38,10 +44,62 @@ export default class Home extends Component {
     }
 
   }
+  navListener(){
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.toolbar_selection');
+    const navLi = document.querySelectorAll('.toolbar_selection li');
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-act');
+        burger.classList.toggle('close');
+    })
+    navLi.forEach((item,i)=>{
+      item.style.animation = `navLi 0.5s ease-in forwards ${i/7 + 1}s`;
+      item.addEventListener('click',()=>{
+        nav.classList.toggle('nav-act');
+        burger.classList.toggle('close');
+      })
+    })
+  }
   render() {
     return (
       <div className = 'home'>
-        <Toolbar/>
+        {/* <Toolbar/> */}
+        <nav className = 'toolbar_nav'>
+           <div className = 'logo'>
+             <img style = {{paddingRight:5}} src = {require('../asset/Zlogos.png')} width='40' height='35'/>
+             <span style = {{color:'#004F87'}}>ac</span>
+             <img style = {{paddingRight:5}} src = {require('../asset/yyy-removebg-preview (1).png')} width='40' height='35'/>
+             <span style = {{color:'#67A7DC'}}>ou</span>
+           </div>
+            
+              <ul className = 'toolbar_selection'>
+                <li>
+                 <i class = 'mdi mdi-home' style={{color:'white',fontSize:'medium',paddingRight:'2px'}}></i><a href='#main'>Home</a>
+                </li>
+                <li>
+                  <i class ='mdi mdi-account-box' style={{color:'white',fontSize:'medium',paddingRight:'2px'}} ></i><a href='#about'>About</a>
+                </li>
+                <li>
+                <i class ='mdi mdi-apps' style={{color:'white',fontSize:'medium',paddingRight:'2px'}} ></i><a href='#project'>Project</a>
+                </li>
+                <li>
+                <i class ='mdi mdi-file-document' style={{color:'white',fontSize:'medium',paddingRight:'2px'}} ></i><a href='#resume'>Resume</a>
+                </li> 
+                <li>
+                <i class ='mdi mdi-phone-classic' style={{color:'white',fontSize:'medium',paddingRight:'2px'}} ></i><a href='#footer'>Contact</a>
+                </li>
+              </ul>
+              
+             
+         
+          <div className = 'burger'>
+                <div className ='line_one'></div>
+                <div className ='line_two'></div>
+                <div className ='line_three'></div>
+
+              </div>
+  
+          </nav>
         <div id = 'main' className = 'first-section'>
           <Content/>
           <Row className = 'buttons' >
