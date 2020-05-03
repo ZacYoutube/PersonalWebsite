9b1a08d77559dda53063d1297b34926658de5397
ZacYou <zyou@calstatela.edu>
2020-05-03 14:48:00 -0700

    final fix

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,7 +6,7 @@ import Content from './Content'
 import Project from './Project';
 import About from './About';
 import Footer from './Footer';
-
+import pdf_file from '../files/Resume Zac You.pdf'
 import {Button,Row,Col} from 'react-bootstrap';
 
 
@@ -17,54 +17,64 @@ export default class Home extends Component {
     this.navListener = this.navListener.bind(this);
   }
   componentDidMount(){
-    this.navListener();
-    this.windowScroll();
+      this.navListener();
+      this.windowScroll(); 
+    
   }
   componentWillUnmount(){
     this.navListener();
+    this.windowScroll();
   }
   windowScroll(){
-    let main = document.getElementById('main');
-    let about = document.getElementById('about');
-    let project = document.getElementById('project');
-    window.onscroll = function(){
-      console.log(window.innerWidth)
-      let distanceToTop = document.documentElement.scrollTop;
-      main.style.opacity = 1 - distanceToTop/1000;
-      if(window.getComputedStyle(main).getPropertyValue("opacity") < 0.2){
-        about.style.opacity = 1 - (distanceToTop/(window.innerWidth<1200?2150:1200) - 0.7)
-        
-      }
-      if(window.getComputedStyle(about).getPropertyValue("opacity")< 0.3){
-       
-        project.style.opacity = 1 - (distanceToTop/(window.innerWidth<=1200?2800:2000) - 0.9 )
-      }
-      // console.log(window.getComputedStyle(about).getPropertyValue("opacity"))
-    
+   
+      let main = document.getElementById('main');
+      let about = document.getElementById('about');
+      let project = document.getElementById('project');
+      window.onscroll = function(){
+        console.log(window.innerWidth)
+        let distanceToTop = document.documentElement.scrollTop;
+        main.style.opacity = 1 - distanceToTop/1000;
+        if(window.getComputedStyle(main).getPropertyValue("opacity") < 0.2){
+          about.style.opacity = 1 - (distanceToTop/(window.innerWidth<1200?2150:1200) - 0.7)
+          
+        }
+        if(window.getComputedStyle(about).getPropertyValue("opacity")< 0.3){
+         
+          project.style.opacity = 1 - (distanceToTop/(window.innerWidth<=1200?2800:2000) - 0.9 )
+        }
+        // console.log(window.getComputedStyle(about).getPropertyValue("opacity"))
+      
+      
     }
+   
 
   }
   navListener(){
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.toolbar_selection');
-    const navLi = document.querySelectorAll('.toolbar_selection li');
-    burger.addEventListener('click',()=>{
-        nav.classList.toggle('nav-act');
-        burger.classList.toggle('close');
-    })
-    navLi.forEach((item,i)=>{
-      item.style.animation = `navLi 0.5s ease-in forwards ${i/7 + 1}s`;
-      item.addEventListener('click',()=>{
-        nav.classList.toggle('nav-act');
-        burger.classList.toggle('close');
+   
+      const burger = document.querySelector('.burger');
+      const nav = document.querySelector('.toolbar_selection');
+      const navLi = document.querySelectorAll('.toolbar_selection li');
+      burger.addEventListener('click',()=>{
+          nav.classList.toggle('nav-act');
+          burger.classList.toggle('close');
       })
-    })
+      navLi.forEach((item,i)=>{
+        item.style.animation = `navLi 0.5s ease-in forwards ${i/7 + 1}s`;
+        item.addEventListener('click',()=>{
+          nav.classList.toggle('nav-act');
+          burger.classList.toggle('close');
+        })
+      })
+    
+    
   }
   render() {
     return (
-      <div className = 'home'>
-        {/* <Toolbar/> */}
-        <nav className = 'toolbar_nav'>
+     
+    
+      
+          <div id = 'home-div' className = 'home'>
+             <nav className = 'toolbar_nav'>
            <div className = 'logo'>
              <img style = {{paddingRight:5}} src = {require('../asset/Zlogos.png')} width='40' height='35'/>
              <span style = {{color:'#004F87'}}>ac</span>
@@ -136,15 +146,22 @@ export default class Home extends Component {
             <h3 style = {{marginBottom:'5%'}} className = 'title'>My Resume</h3>
             <img className = 'title-decor' src = {require('../asset/leaf-removebg-preview.png')}/>
             <br/>
-            <img className = 'resume-2' src = {require('../files/resume1.png')} width = '45%' height = '800px'/>
-            <img className = 'resume-1' src = {require('../files/resume2.png')} width = '45%' height = '800px'/>
-
+            <img className = 'resume-2' src = {require('../files/Resume Zac You-1.png')} width = '45%' height = '800px'/>
+            
           
+        </div>
+        <div className = 'download-button'>
+          <Button href = {pdf_file} download = 'Resume Zac You.pdf' style = {{backgroundColor: '#088A68', border:'none'}}>Download My Resume</Button>
         </div>
         <div id = 'footer'>
           <Footer/>
         </div>
       </div>
+  
+  
+       
+  
+     
     )
   }
 }
